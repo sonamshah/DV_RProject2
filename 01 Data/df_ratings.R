7fc6eba45403fbc632eca25a2e34935fcee52358
unknown <theodorahcheng@gmail.com>
2015-02-14 17:09:36 -0600

    sales data

--- 01 Data/df_ratings.R
+++
@@ -1,1 +0,0 @@
-df_ratings <- data.frame(eval(parse(text=substring(getURL(URLencode('http://129.152.144.84:5001/rest/native/?query="select * from sleeplevels"'), httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_srs2994', PASS='orcl_srs2994', MODE='native_mode', MODEL='model', returnFor = 'R', returnDimensions = 'False'), verbose = TRUE), 1, 2^31-1))))
