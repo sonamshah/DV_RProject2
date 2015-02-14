062af5d2c572447107c8eef52d8263da7c4b6a18
unknown <theodorahcheng@gmail.com>
2015-02-13 19:16:46 -0600

    new df

---
+++ 01 Data/df_background.R
@@ -0,0 +1,1 @@
+df_background <- data.frame(eval(parse(text=substring(getURL(URLencode('http://129.152.144.84:5001/rest/native/?query="select * from background"'), httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_thc359', PASS='orcl_thc359', MODE='native_mode', MODEL='model', returnFor = 'R', returnDimensions = 'False'), verbose = TRUE), 1, 2^31-1))))
