b6acc5eae7e0e62dd0f168377616beb47ddf2af7
sonamshah <sonam.shah17@yahoo.com>
2015-02-16 14:29:24 -0600

    perfected pie chart with data wranglin

--- 01 Data/df_pmttype_mktshare.R
+++ 01 Data/df_pmttype_mktshare.R
@@ -2,7 +2,10 @@ library(RCurl)
 library(ggplot2)
 library(tidyr)
 library(dplyr)
+
 df_sales <- data.frame(eval(parse(text=substring(getURL(URLencode('http://129.152.144.84:5001/rest/native/?query="select * from sales"'), httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_thc359', PASS='orcl_thc359', MODE='native_mode', MODEL='model', returnFor = 'R', returnDimensions = 'False'), verbose = TRUE), 1, 2^31-1))))
+
+df_sales %>% select(PAYMENT_TYPE) %>% ggplot(aes(x=PAYMENT_TYPE)) + geom_histogram()
 
 # pie chart of payment type
 a <- nrow(subset(df_sales, PAYMENT_TYPE == 'Mastercard'))
@@ -12,4 +15,9 @@ d <- nrow(subset(df_sales, PAYMENT_TYPE == 'Diners'))
 
 slices <- c(a, b, c, d)
 lbls <- c('Mastercard', 'Visa', 'Amex', 'Diners')
-pie(slices, labels = lbls, main = 'International Market Share of Payment Types')
+ppct <- round(slices/sum(slices)*100)
+lbls <- paste(lbls, pct) # add percents to labels 
+lbls <- paste(lbls,"%",sep="") # ad % to labels 
+pie(slices,labels = lbls, col=rainbow(length(lbls)),
+    main='International Market Share of Payment Types')
+
