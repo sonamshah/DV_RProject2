e8cef8125bdb58e93e0af3cf4c29a1cd96db6508
unknown <theodorahcheng@gmail.com>
2015-02-16 15:45:30 -0600

    please be last push

--- 02 Data Wrangling/df_pmttype_mktshare.R
+++ 02 Data Wrangling/df_pmttype_mktshare.R
@@ -5,6 +5,8 @@ library(dplyr)
 
 df_sales <- data.frame(eval(parse(text=substring(getURL(URLencode('http://129.152.144.84:5001/rest/native/?query="select * from sales"'), httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_thc359', PASS='orcl_thc359', MODE='native_mode', MODEL='model', returnFor = 'R', returnDimensions = 'False'), verbose = TRUE), 1, 2^31-1))))
 
+df_sales %>% select(PAYMENT_TYPE) %>% group_by(PAYMENT_TYPE) %>% count(PAYMENT_TYPE)
+
 # pie chart of payment type
 a <- nrow(subset(df_sales, PAYMENT_TYPE == 'Mastercard'))
 b <- nrow(subset(df_sales, PAYMENT_TYPE == 'Visa'))
