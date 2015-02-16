6e5672d026837a0317810239682e19a3d4d87cd8
unknown <theodorahcheng@gmail.com>
2015-02-16 14:51:01 -0600

    so many pushes -_-

--- 01 Data/df_price_by_country.R
+++ 01 Data/df_price_by_country.R
@@ -6,5 +6,6 @@ library(dplyr)
 df_sales <- data.frame(eval(parse(text=substring(getURL(URLencode('http://129.152.144.84:5001/rest/native/?query="select * from sales"'), httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_thc359', PASS='orcl_thc359', MODE='native_mode', MODEL='model', returnFor = 'R', returnDimensions = 'False'), verbose = TRUE), 1, 2^31-1))))
 
 # mean price by country
-df_sales %>% select(PAYMENT_TYPE, COUNTRY, PRICE) %>% group_by(COUNTRY) %>% summarise(mean_price = mean(PRICE)) %>% ggplot(aes(x=COUNTRY, y=mean_price)) + geom_point()
+df_sales %>% select(PAYMENT_TYPE, COUNTRY, PRICE) %>% group_by(COUNTRY) %>% summarise(mean_price = mean(PRICE)) 
+#%>% ggplot(aes(x=COUNTRY, y=mean_price)) + geom_point()
 #df_sales <- df_sales + theme(axis.text.x=element_text(angle=90, size=8, vjust=0.5))
--- 03 Visualizations/vis_price_by_country.R
+++ 03 Visualizations/vis_price_by_country.R
@@ -5,5 +5,11 @@ library(dplyr)
 
 df_sales <- data.frame(eval(parse(text=substring(getURL(URLencode('http://129.152.144.84:5001/rest/native/?query="select * from sales"'), httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_thc359', PASS='orcl_thc359', MODE='native_mode', MODEL='model', returnFor = 'R', returnDimensions = 'False'), verbose = TRUE), 1, 2^31-1))))
 
-#df_sales %>% select(PAYMENT_TYPE) 
-#%>% ggplot(aes(x=PAYMENT_TYPE)) +geom_histogram()
+
+# mean price by country
+mean_price <- tapply(df_sales$PRICE, df_sales$COUNTRY, mean)
+COUNTRY <- factor(levels(df_sales$COUNTRY), levels=levels(df_sales$COUNTRY))
+g <- qplot(COUNTRY, mean_price, geom="bar", stat="identity", fill = factor(COUNTRY))
+g <- g + labs(x="Country", y="Mean Price", title="Country Expenses")
+g <- g + theme(axis.text.x=element_text(angle=90, size=8, vjust=0.5))
+g + theme(legend.position="none")
